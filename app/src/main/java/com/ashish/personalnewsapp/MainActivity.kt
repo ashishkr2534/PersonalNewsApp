81b87ed6734c7bce20780af39edb0b9089c0d85f
Ashish Kumar <ashishkr2534@outlook.com>
2025-05-02 17:01:35 +0530

    dependencies added

--- app/src/main/java/com/ashish/personalnewsapp/MainActivity.kt
+++ app/src/main/java/com/ashish/personalnewsapp/MainActivity.kt
@@ -6,8 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "HelloJi  $name!",
         modifier = modifier
     )
 }
